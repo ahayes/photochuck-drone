1fc54e8a4fcf58ac6cffdb5372f8a8363304f601
Wesley Ellis <tahnok@gmail.com>
2013-11-02 15:35:14 -0400

    code cleanup

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
 var library = require("serialport");
 
-
-
-var serialPort = new library.SerialPort("/dev/tty.usbmodem1411", {
+var serialPort = new library.SerialPort("/dev/tty.usbmodem1421", {
   baudrate: 19200,
-  parser: library.parsers.readline("\n")
+    parser: library.parsers.readline("\n")
 });
-
-
-
 
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
-
-//client.takeoff();
-
-
 
 var stdin = process.stdin;
 
@@ -29,10 +20,13 @@ stdin.resume();
 // i don't want binary, do you?
 stdin.setEncoding( 'utf8' );
 
-var movable = true;
+var movement = false;
 var last_button_time = new Date().getTime();
-var last_button = false;
 
+    function strip(str) {
+      if(str === undefined) { return str; }
+      return str.replace(/\r/g, '');
+    }
 serialPort.on("open", function () {
   console.log('open');
   serialPort.on('data', function(raw_data) {
@@ -43,54 +37,59 @@ serialPort.on("open", function () {
     var accelY = data[3];
     var accelZ = data[4];
     var zButton = data[5];
-    var cButton = data[6];
+    var cButton = strip(data[6]);
 
     var current_time = new Date().getTime();
-    if(zButton === "1")
-    {
-      if (current_time > (last_button_time + 200))
-      {
-        movable = !movable;
+    if(zButton === "1") {
+      if (current_time > (last_button_time + 200)) {
+        movement = !movement;
       }
       last_button_time = current_time;
     }
+    if(!movement){
+      stop();
+    } else {
+      var xAmmount = mungeAnalog(analogX);
+      var yAmmount = mungeAnalog(analogY);
+      console.log(xAmmount);
 
-
-    var xAmmount = mungeAnalog(analogX);
-    var yAmmount = mungeAnalog(analogY);
-
-
-    if (!movable || (xAmmount === 0 && yAmmount === 0))
-    {
+      if (xAmmount === 0 && yAmmount === 0) {
         stop();
-    }
-    else
-    {
-      moveFront(yAmmount);
-      yawRight(xAmmount);
+      } else {
+        moveFront(yAmmount);
+        yawRight(xAmmount);
+      }
+
+      var height = mungeAccel(accelY);
+      if (height === 0) {
+        stop();
+      } else {
+        flyUp(height);
+      }
     }
 
-
-    //var x = mungeAnalog(analogX);
-    //var y = mungeAnalog(analogY);
-    //console.log("x: " + x + " y: " + y);
-    //console.log(mungeAccel(accelX));
   });
 });
 
 var mungeAnalog = function(analog){
-    var temp = analog / 10;
-    temp = temp - 12.5;
-    temp = truncate(temp);
-    temp = temp / 10;
-    return temp;
+  var temp = analog / 10;
+  temp = temp - 12.5;
+  temp = truncate(temp);
+  temp = temp / 10;
+  return temp;
 };
 
+//FIXME
 var mungeAccel = function(accel){
+  if(accel > 400 && accel < 600) {
+    return 0;
+  }
   var temp = accel / 20;
   temp = temp - 25;
   temp = truncate(temp);
   temp = temp / 10;
+  offset = (temp < 0) ? 0.5 : -0.5;
+  temp = temp + offset;
   return temp;
 };
 
@@ -110,109 +109,72 @@ stdin.on( 'data', function( key ){
   var output = '-';
   // write the key to stdout all normal like
 
-  switch(key)
-  {
-    case 'w':
-      output = moveFront();
-      break;
-
-    case 'x':
-      output = moveBack();
-      break;
-
-    case 'a':
-      output = moveLeft();
-      break;
-
-    case 'd':
-      output = moveRight();
-      break;
-
-
-  case 'i':
-      output = flyUp();
-      break;
-
-    case 'k':
-      output = flyDown();
-      break;
-
-    case 'j':
-      output = yawLeft();
-      break;
-
-    case 'l':
-      output = yawRight();
-      break;
-
-
+  switch(key) {
   case '0':
-      output = droneOn();
-      break;
+    output = droneOn();
+    break;
 
-    case '9':
-      output = droneOff();
-      break;
+  case '9':
+    output = droneOff();
+    break;
 
   case 's':
-      output = stop();
-      break;
-
-
-
-
-    default:
-      output = '-';
-
-
+    output = stop();
+    break;
+  default:
+    output = '-';
   }
-
-
-
-  if(output != "-")
-  {
+  if(output != "-") {
     process.stdout.write( "Pressed: " + output + '\n');
   }
 
-
 });
-
-
-
-
-
-
 
 
 var rotRate = 0.25;
 var horzRate = 0.2;
 var vertRate = 0.1;
 
-function yawLeft(val){ client.counterClockwise(rotRate); return "Rot CW"; }
+function yawRight(val){
+  client.clockwise(val);
+  console.log("moving CCW: " + val);
+  return "Rot CCW";
+}
 
-function yawRight(val){ client.clockwise(val); console.log("moving CCW: " + val); return "Rot CCW"; }
+function moveFront(val){
+  client.front(val);
+  console.log("moving front: " + val);
+  return "Move Forward";
+}
 
-function moveFront(val){ client.front(val); console.log("moving front: " + val); return "Move Forward"; }
+function moveRight(val){
+  client.left(val);
+  console.log("moving right: " + val);
+  return "Move Left";
+}
 
-function moveBack(val){ client.back(horzRate); return "Move Backward"; }
+function flyUp(val){
+  client.up(val);
+  return "Fly Up";
+}
 
-function moveLeft(val){ client.left(horzRate); return "Move Left"; }
+function droneOn(){
+  console.log("Launch drone");
+  client.takeoff();
+  return "Launch drone";
+}
 
-function moveRight(val){ client.right(horzRate); return "Move Right"; }
+function droneOff(){
+  console.log("Land drone");
+  client.land();
+  return "Land the drone";
+}
 
-function flyUp(val){ client.up(vertRate); return "Fly Up"; }
-
-function flyDown(val){ client.down(vertRate); return "Fly Down"; }
-
-
-function droneOn(){ client.takeoff(); return "Launch drone"; }
-
-function droneOff(){ client.land(); return "Land the drone"; }
-
-
-function stop(){ client.stop(); console.log("stopping..."); return "Stop the drone"; }
-
-
+function stop(){
+  client.stop();
+  console.log("stopping...");
+  return "Stop the drone";
+}
 
 /////////////////////////////////////////////////////////////////////
 //PICTURES
